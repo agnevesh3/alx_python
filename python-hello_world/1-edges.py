5ff68d6cf5a711690f0c42e414afd1aaba5dffcb
agnaldo <agnevesh3@yahoo.com>
2023-07-22 15:00:26 +0200

    snyc

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -6,3 +6,11 @@ middle_word = word[1:8]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
+
+word_2 = "school"
+word_first_3 = word_2[:3]
+word_last_2 = word_2[4:]
+middle_word = word_2[1:5]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
