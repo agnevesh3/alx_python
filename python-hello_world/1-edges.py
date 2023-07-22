efcc8c0dd3d41f4b32265fd7af93955e89d37111
agnaldo <agnevesh3@yahoo.com>
2023-07-22 15:18:32 +0200

    snyc

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word1 = "Holberton"
 
-# Extracting substrings from word1 and word2
-word1_first_3, word1_last_2, word1_middle = word1[:3], word1[-2:], word1[1:-1]
-word2_first_3, word2_last_2, word2_middle = word2[:3], word2[-2:], word2[1:-1]
+word1_first_3 = word1[:3]
+word1_last_2 = word1[-2:]
+word1_middle = word1[1:-1]
 
 print(
     "First 3 letters of word1: {}\nLast 2 letters of word1: {}\nMiddle word of word1: {}".format(
