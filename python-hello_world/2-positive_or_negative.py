7e64b099ba06fb9bf3cf6863d75111d266a944ac
agnaldo <agnevesh3@yahoo.com>
2023-07-22 15:29:10 +0200

    sny

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,8 +1,11 @@
 ##Positive anything is better than negative nothing
 import random
+
 number = random.randint(-10, 10)
 # YOUR CODE HERE
-if number>=0:
-      print(number, "is positive")
+if number > 0:
+    print(number, "is positive")
+elif number == 0:
+    print(number, "is zero")
 else:
-     print(number, "is negative")
+    print(number, "is negative")
