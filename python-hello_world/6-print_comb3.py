ef576a2aa0318dff6563e78555386c712fbccfbd
agnaldo <agnevesh3@yahoo.com>
2023-07-22 16:00:33 +0200

    snyc

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,13 +1,9 @@
 ##Inveting is combination of brains and materials.The more brains you use, the less material you need
 
-for i in range(0,10):
-    for j in range(0,10):
-        if(i!=j and i<j):
-            if i==8:
-                print(f"{i}{j}")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i != j and i < j:
+            if i == 8:
+                print("{}{}".format((i), (j)))
             else:
-                print(f"{i}{j}", end=", ")
-
-
-    
-
+                print("{}{}".format((i), (j)), end=", ")
