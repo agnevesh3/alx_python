c172e5a8ed4d51d4d9b4c705363d1c28ddac55ba
agnaldo <agnevesh3@yahoo.com>
2023-07-24 19:24:48 +0200

    sync

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -17,7 +17,7 @@ def validate_password(password):
     return True
 
 
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+# print(validate_password("Password123"))
+# print(validate_password("abc123"))
+# print(validate_password("Password 123"))
+# print(validate_password("password123"))
