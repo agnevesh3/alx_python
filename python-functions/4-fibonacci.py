65f0c0febe413730adb6d729ffe893572c7a5ff3
agnaldo <agnevesh3@yahoo.com>
2023-07-24 19:45:19 +0200

    sync

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
-    if n in {0, 1}:
-        return [n]  # Return a list with a single element if n is 0 or 1
+    if n in {0}:
+        return []  # Return an empty list if n is  0
+    if n in {1}:
+        return [0]  # Return an empty list if n is 1
 
     # Create a list to store Fibonacci sequence
     fib_seq = [0, 1]
@@ -16,6 +18,6 @@ def fibonacci_sequence(n):
 
 # Test the function
 # print(fibonacci_sequence(6))
-# print(fibonacci_sequence(1))
+print(fibonacci_sequence(6))
 # print(fibonacci_sequence(0))
 # print(fibonacci_sequence(20))
