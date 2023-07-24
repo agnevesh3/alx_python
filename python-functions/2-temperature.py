726523d9c786595f69e4507059453b0e514a9502
agnaldo <agnevesh3@yahoo.com>
2023-07-24 19:33:49 +0200

    sync

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,10 +2,10 @@
 def convert_to_celsius(fahrenheit):
     # convert_to_celsius = __import__("operator").
     convert_to_celsius = (float(fahrenheit) - 32) * (5 / 9)
-    return convert_to_celsius
+    return round(convert_to_celsius, 2)
 
 
-# print(convert_to_celsius(100))
+# print(convert_to_celsius(459.67))
 # print(convert_to_celsius(-40))
 # print(convert_to_celsius(-459.67))
 # print(convert_to_celsius(32))
