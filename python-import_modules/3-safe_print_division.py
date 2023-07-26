71607ab6811b5bd115c8e7cd8bd7badd82e1b186
agnaldo <agnevesh3@yahoo.com>
2023-07-26 20:58:56 +0200

    sync

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -15,3 +15,15 @@ def safe_print_division(a, b):
 a = 10
 b = 2
 result = safe_print_division(a, b)
+a = 10
+b = -2
+result = safe_print_division(a, b)
+a = 0
+b = 2
+result = safe_print_division(a, b)
+a = 10
+b = 0
+result = safe_print_division(a, b)
+a = 0
+b = 0
+result = safe_print_division(a, b)
