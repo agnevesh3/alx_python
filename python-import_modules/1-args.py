2c2370dba4d451aaf0ef5c6c2a9a5b00a93eb415
agnaldo <agnevesh3@yahoo.com>
2023-07-26 17:04:09 +0200

    sync

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,10 +2,10 @@ if __name__ == "__main__":
 
     def argument(*args):
         if len(args) == 0:
-            result = print("0 arguments")
+            result = print("0 arguments:")
         elif len(args) == 1:
-            result = print("1 argument")
+            result = print("1 argument:")
         else:
-            result = print(len(args), "arguments")
+            result = print(len(args), "arguments:")
         for index, arg in enumerate(args, start=1):
             print("{}: {}".format(index, arg))
