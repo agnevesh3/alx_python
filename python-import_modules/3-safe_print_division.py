f35a144cf7bbe0f0213046d87d889e0d6e1a8efc
agnaldo <agnevesh3@yahoo.com>
2023-07-26 21:19:50 +0200

    sync

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,20 +9,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result:", division)
         print("{} / {} = {}".format(a, b, division))
-
-
-a = 10
-b = 2
-result = safe_print_division(a, b)
-a = 10
-b = -2
-result = safe_print_division(a, b)
-a = 0
-b = 2
-result = safe_print_division(a, b)
-a = 10
-b = 0
-result = safe_print_division(a, b)
-a = 0
-b = 0
-result = safe_print_division(a, b)
