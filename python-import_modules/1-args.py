e2496b6fc9353f1cf8f18af0a58d7dc5cfe9d733
agnaldo <agnevesh3@yahoo.com>
2023-07-26 22:02:17 +0200

    sync 2

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,24 @@
-if __name__ == "__main__":
+def argument(*args):
+    if len(args) == 0:
+        result = print("0 arguments.")
+    elif len(args) == 1:
+        result = print("1 argument:")
+    else:
+        result = print(len(args), "arguments:")
 
-    def argument(*args):
-        if len(args) == 0:
-            result = print("0 arguments.")
-        elif len(args) == 1:
-            result = print("1 argument:")
-        else:
-            result = print(len(args), "arguments:")
-        for index, arg in enumerate(args, start=1):
-            print("{}: {}".format(index, arg))
-        return args
+    for index, arg in enumerate(args, start=1):
+        print("{}: {}".format(index, arg))
+
+    return args
+
+
+if __name__ == "__main__":
+    import sys
+
+    argument(*sys.argv[1:])
 
 
 argument("Hello")
 argument("Hello", "Holberton")
-# argument()
-# argument(98, "battery", "street")
+argument()
+argument(98, "battery", "street")
