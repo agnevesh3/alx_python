7c432c1da2f7502fccd7695e88135c0029a7b227
agnaldo <agnevesh3@yahoo.com>
2023-07-26 20:24:31 +0200

    sync

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,8 +3,11 @@
 
 def safe_print_division(a, b):
     try:
-        division = a // b
+        division = a / b
     except ZeroDivisionError:
         division = None
+    except NameError:
+        division = None
     finally:
+        print("Inside result:", division)
         print("{} / {} = {}".format(a, b, division))
