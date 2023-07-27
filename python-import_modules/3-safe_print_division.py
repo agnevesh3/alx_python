cc94d189c1faaabfeefe3d2c898c273ccc1deaed
agnaldo <agnevesh3@yahoo.com>
2023-07-27 12:09:56 +0200

    sync

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,7 +5,8 @@ def safe_print_division(a, b):
     try:
         division = a / b
     except ZeroDivisionError:
-        division == None
+        division = None
+        return division
     else:
         return division
     finally:
