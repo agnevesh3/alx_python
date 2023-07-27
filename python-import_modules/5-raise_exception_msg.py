7bc29b375d1384bdc0fd384b40182ebfee672c68
agnaldo <agnevesh3@yahoo.com>
2023-07-27 22:01:48 +0200

    sync

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     try:
-        print("C is fun")
+        print(message="")
     except:
-        print("C is fun")
+        print(message="")
+
+
+if __name__ == "__main__":
+    message = "C is fun"
+
+if __name__ == "__main__":
+    message = "Python is cool"
+
+if __name__ == "__main__":
+    message = ""
