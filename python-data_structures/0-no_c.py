973f5fca3c6c8e3e15bd0855e747c2b91e88dfd9
agnaldo <agnevesh3@yahoo.com>
2023-07-28 21:31:06 +0200

    sync

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    my_list = []
+    for i in my_string:
+        my_list.append(i)
+    if "c" in my_list:
+        my_list.remove("c")
+    elif "C" in my_list:
+        my_list.remove("C")
+    for i in my_list:
+        result = print("{}".format(i))
+    return result
+
+
+no_c("Holberton School")
