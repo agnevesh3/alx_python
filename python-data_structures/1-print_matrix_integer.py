295d95e5e0b0e2627370fa55f786d6a74117149d
agnaldo <agnevesh3@yahoo.com>
2023-07-30 19:56:11 +0200

    sync

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
-    matrix_list = []
-    matrix_list.append(matrix)
+    for i in matrix:
+        for j in i:
+            print("{}".format(j), end="")
+
+
+matrix = [[1, 2, 3], [2, 4, 5], [9, 8, 7]]
+print_matrix_integer(matrix)
