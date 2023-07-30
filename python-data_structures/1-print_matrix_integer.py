12a5ea9c6b1dc6a3c593f71b2f982d808bf1b1e3
agnaldo <agnevesh3@yahoo.com>
2023-07-30 20:23:27 +0200

    sync

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
-    for i in matrix:
-        print("[{}]".format(i), end="")
-
-
-matrix = [[1, 2, 3], [2, 4, 5], [9, 8, 7]]
-print_matrix_integer(matrix)
+    for row in matrix:
+        row_str = " ".join(
+            map(str, row)
+        )  # Convert each element in the row to a string and join them with spaces
+        print(row_str + "$")
