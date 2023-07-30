b30e824d98072c34b140999630fe19c6f9d0e851
agnaldo <agnevesh3@yahoo.com>
2023-07-30 17:40:46 +0200

    sync

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -3,10 +3,13 @@ def no_c(my_string):
     my_list = []
     for i in my_string:
         my_list.append(i)
-    if "c" in my_list:
-        my_list.remove("c")
-    elif "C" in my_list:
-        my_list.remove("C")
+    if "C" in my_list:
+        my_string = my_list.remove("C")
+        if "c" in my_list:
+            my_string = my_list.remove("c")
     for i in my_list:
-        result = print("{}".format(i), end="")
-    return my_list
+        print("{}".format(i), end="")
+    return my_string
+
+
+no_c("chiCago")
