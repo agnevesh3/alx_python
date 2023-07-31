9885ddf11084d70ee2a03c780b45132d24d82ce9
agnaldo <agnevesh3@yahoo.com>
2023-07-31 20:40:48 +0200

    sync

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_list = []
-    for i in set_1 and set_2:
-        if i == i in set_1 and set_2:
-            new_list.append(i)
-    print(new_list)
+    common_list = []
+    for i in set_1:
+        if i in set_2:
+            common_list.append(i)
+    return common_list
