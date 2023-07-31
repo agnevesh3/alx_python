0bf479411c2e241ea4cfe2fdad0e2e4780bc8767
agnaldo <agnevesh3@yahoo.com>
2023-07-31 22:22:50 +0200

    sync

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -2,6 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     """Update or add key/value in a dictionary"""
     a_dictionary[key] = value
+    return a_dictionary
 
 
 def print_sorted_dictionary(my_dict):
