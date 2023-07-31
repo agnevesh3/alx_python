44fba3351254f374b4eda44368340c37ed73b444
agnaldo <agnevesh3@yahoo.com>
2023-07-31 18:11:31 +0200

    sync

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
-    result_matrix = []
+    new_matrix = []
     for row in matrix:
         row_squares = [num**2 for num in row]
-        result_matrix.append(row_squares)
-    print(result_matrix)
-    print(matrix)
+        new_matrix.append(row_squares)
+    return new_matrix, matrix
