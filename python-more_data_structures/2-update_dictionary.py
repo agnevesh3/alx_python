261450024ba5fb542d39888d9dc2acb839e0b7a9
agnaldo <agnevesh3@yahoo.com>
2023-07-31 21:14:02 +0200

    sync

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    key = int(key)
-    if key in a_dictionary:
-        value.update()
-        if key not in a_dictionary:
+def print_sorted_dictionary(my_dict):
+    """Print sorted dictionary"""
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
 
+
+my_dict = {"a": "a", "b": "b", "c": "c", "d": "d", "e": "e"}
+key = "a"
+value = "A"
+new_dict = update_dictionary(my_dict, key, value)
+print_sorted_dictionary(new_dict)
+print("xx")
+print_sorted_dictionary(my_dict)
