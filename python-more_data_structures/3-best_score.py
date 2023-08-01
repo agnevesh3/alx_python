65d9b4b145add318395a2245e8aa1f8a55311dca
agnaldo <agnevesh3@yahoo.com>
2023-08-01 13:34:41 +0200

    sync

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,12 @@
+def best_score(a_dictionary):
+    # Initialize variables to keep track of the maximum score and its corresponding key
+    max_score = None
+    max_key = None
+
+    for key, value in a_dictionary.items():
+        if isinstance(value, int):  # Check if the value is an integer
+            if max_score is None or value > max_score:
+                max_score = value
+                max_key = key
+
+    return max_key
