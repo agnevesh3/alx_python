ae07c24fd9cfb1297898ab72bebce5cc03f98240
agnaldo <agnevesh3@yahoo.com>
2023-08-02 17:42:00 +0200

    sync

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    ### this block will declare the size of square
+    """this block will declare the size of square"""
+
     def __init__(self, size):
+        """A simple module that return the size of a square"""
         try:
             self.__size = size
         except Exception as e:
