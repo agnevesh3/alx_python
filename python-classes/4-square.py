2ec844900e0f3a2a1beb2035026a5db747baf0db
agnaldo <agnevesh3@yahoo.com>
2023-08-02 21:04:00 +0200

    sync

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,4 +1,48 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int, optional): The size of the square. Default is 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer.")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of a square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """Print the square using '#' characters."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
