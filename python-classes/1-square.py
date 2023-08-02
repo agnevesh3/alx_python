6c2eba42a5520b6d337dbcd6fa28de89e7b6a15f
agnaldo <agnevesh3@yahoo.com>
2023-08-02 19:43:16 +0200

    sync

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,13 @@
+"""
+This module defines the Square class.
+
+The Square class represents a square with a given size.
+
+Attributes:
+    __size (int): The size of the square.
+"""
+
+
 class Square:
     """
     This class represents a square.
