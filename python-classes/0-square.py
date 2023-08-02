458944d0bb5590bb9104f8f7760a93c8afb0ff4f
agnaldo <agnevesh3@yahoo.com>
2023-08-02 17:57:10 +0200

    sync

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,5 +5,8 @@ class Square:
     """
 
     def __init__(self, size):
-        """initializes the square data"""
+        """Why size is private attribute?
+
+        The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. One way to have the control is to keep it privately. You will see in next tasks how to get, update and validate the size value.
+        """
         self.__size = size
