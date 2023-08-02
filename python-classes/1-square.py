364d3e613a952abbd0fd012cd1c2e0ee6c570a47
agnaldo <agnevesh3@yahoo.com>
2023-08-02 20:28:28 +0200

    sync

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -29,15 +29,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """
-        Calculate the area of a square.
-
-        Parameters:
-            size (int): The size of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size**2
