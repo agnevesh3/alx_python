fd1464a1f9fe50d2a206bb3d4f873ce61cc98626
agnaldo <agnevesh3@yahoo.com>
2023-08-02 21:00:56 +0200

    sync

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,49 @@
-              
+"""
+This module defines the Square class.
+
+The Square class represents a square with a given size.
+
+Attributes:
+    __size (int): The size of the square.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int, optional): The size of the square. Default is 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer.")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of a square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size**2
