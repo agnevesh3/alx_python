c6055a9715b048e821218e400a19b4689d622acf
agnaldo <agnevesh3@yahoo.com>
2023-08-06 19:41:32 +0200

    sync

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -15,12 +15,3 @@ def inherits_from(obj, a_class):
     bool: True if the object is an instance of a class that inherited from the specified class; otherwise, False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
