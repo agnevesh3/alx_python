611e193abce9a6c27c2a9d928cf7166b247f969c
agnaldo <agnevesh3@yahoo.com>
2023-08-06 14:39:46 +0200

    sync

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
+# type_checker.py
+
+
+class TypeChecker:
     """
-    Check if the given object is exactly an instance of the specified class.
+    A class to check if an object is exactly an instance of a specified class.
 
-    Parameters:
-        obj (object): The object to be checked.
-        a_class (class): The class to compare with.
-
-    Returns:
-        bool: True if the object is an instance of the specified class; otherwise, False.
-
-    Examples:
-        >>> a = 1
-        >>> is_same_class(a, int)
-        True
-
-        >>> is_same_class(a, float)
-        False
-
-        >>> is_same_class(a, object)
-        True
-
-    Note:
-        This function uses the __class__ attribute of the object to check if it is an instance of the
-        specified class. It returns True if the object's class is the same as the specified class.
-        If the object is not an instance of the specified class, it returns False.
+    This class provides a method, is_same_class, that checks if the given object is an instance
+    of the specified class. It uses the __class__ attribute of the object to get its class and
+    compares it with the specified class to determine if they match.
     """
-    return obj.__class__ is a_class
+
+    def is_same_class(self, obj, a_class):
+        """
+        Check if the given object is exactly an instance of the specified class.
+
+        Parameters:
+            obj (object): The object to be checked.
+            a_class (class): The class to compare with.
+
+        Returns:
+            bool: True if the object is an instance of the specified class; otherwise, False.
+
+        Examples:
+            >>> checker = TypeChecker()
+            >>> a = 1
+            >>> checker.is_same_class(a, int)
+            True
+
+            >>> checker.is_same_class(a, float)
+            False
+
+            >>> checker.is_same_class(a, object)
+            True
+
+        Note:
+            This method uses the __class__ attribute of the object to check if it is an instance of the
+            specified class. It returns True if the object's class is the same as the specified class.
+            If the object is not an instance of the specified class, it returns False.
+        """
+        return obj.__class__ is a_class
