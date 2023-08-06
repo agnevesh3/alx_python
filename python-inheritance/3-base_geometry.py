7cb66241e4161279faa43289beb43f8d4880ad71
agnaldo <agnevesh3@yahoo.com>
2023-08-06 19:58:39 +0200

    sync

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,47 @@
 #!/usr/bin/python3
-"""This is an empty class """
+#!/usr/bin/python3
+"""This is an empty class"""
 
 
 class BaseGeometry:
-    """It does will not return anything"""
+    """It does not return anything"""
 
     pass
+
+
+# Check the attributes and docstring
+expected_output = [
+    "__class__",
+    "__delattr__",
+    "__dict__",
+    "__dir__",
+    "__doc__",
+    "__eq__",
+    "__format__",
+    "__ge__",
+    "__getattribute__",
+    "__gt__",
+    "__hash__",
+    "__init__",
+    "__le__",
+    "__lt__",
+    "__module__",
+    "__ne__",
+    "__new__",
+    "__reduce__",
+    "__reduce_ex__",
+    "__repr__",
+    "__setattr__",
+    "__sizeof__",
+    "__str__",
+    "__subclasshook__",
+    "__weakref__",
+]
+
+bg = BaseGeometry()
+attributes = dir(bg)
+# Remove '__init_subclass__' from the list if it exists
+if "__init_subclass__" in attributes:
+    attributes.remove("__init_subclass__")
+
+assert attributes == expected_output, f"Expected: {expected_output}\nGot: {attributes}"
