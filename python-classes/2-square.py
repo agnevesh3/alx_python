f77490bf3c1d156dac99b14a5e4d39a0e365c210
agnaldo <agnevesh3@yahoo.com>
2023-08-06 22:18:30 +0200

    sync

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,8 @@
 """
 This module defines the Square class.
 
-The Square class represents a square with a given size.
+The Square class represents a square
+ with a given size.
 
 Attributes:
     __size (int): The size of the square.
@@ -13,7 +14,8 @@ class Square:
     This class represents a square.
 
     Attributes:
-        __size (int): The size of the square.
+        __size (int): The size of
+        the square.
     """
 
     def __init__(self, size=0):
