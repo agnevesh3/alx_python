0c83b547a33663b392e35588868416ad2d7074db
agnaldo <agnevesh3@yahoo.com>
2023-08-11 16:56:49 +0200

    sync

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"    - type: {type(response.text)}")
+    print(f"    - content: {response.text}")
+else:
+    print(f"Error: Unable to fetch data. Status code: {response.status_code}")
